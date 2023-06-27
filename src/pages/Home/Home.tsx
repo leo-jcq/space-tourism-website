97565e000edca5f2c2741964f875a244d047da59
Léo Jacquet <leojacquet2003@gmail.com>
2023-06-27 13:12:38 +0200

    💄 finish homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,25 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.scss';
 
 const Home: FC = () => {
-    return <div className="home">Home</div>;
+    return (
+        <div className="home">
+            <div className="text">
+                <h5>So, you want to travel to</h5>
+                <h1>Space</h1>
+                <p>
+                    Let’s face it; if you want to go to space, you might as well genuinely go to
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax
+                    because we’ll give you a truly out of this world experience!
+                </p>
+            </div>
+
+            <Link to="destination" className="explore">
+                Explore
+            </Link>
+        </div>
+    );
 };
 
 export default Home;
