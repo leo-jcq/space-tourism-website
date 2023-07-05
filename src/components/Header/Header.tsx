3ea52273c76eecf7d96050379534e72914ff2cae
Léo Jacquet <leojacquet2003@gmail.com>
2023-07-05 03:49:21 +0200

    ♿️ add link to homepage in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/imgs/logo.svg';
 import NavBar from '../NavBar/NavBar';
 import './Header.scss';
@@ -6,7 +7,10 @@ import './Header.scss';
 const Header: FC = () => {
     return (
         <header className="header">
-            <img src={logo} alt="Logo" className="logo" />
+            <Link to="/">
+                <img src={logo} alt="Logo" className="logo" />
+            </Link>
+
             <NavBar />
         </header>
     );
